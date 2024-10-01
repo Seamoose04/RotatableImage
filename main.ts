ba2ff7d635e8a284339102e829e361fe050b8b3a
Seamus Johnson <SMJohnson6@student.fullsail.edu>
2024-10-01 12:30:35 -0400

    loaded original code

--- main.ts
+++ main.ts
@@ -1,24 +1,13 @@
-/**
- * This is the main file for your project.
- *
- * Create images, tilemaps, animations, and songs using the
- * asset explorer in VS Code. You can reference those assets
- * using the tagged templates on the assets namespace:
- *
- *     assets.image`myImageName`
- *     assets.tilemap`myTilemapName`
- *     assets.tile`myTileName`
- *     assets.animation`myAnimationName`
- *     assets.song`mySongName`
- *
- * New to MakeCode Arcade? Try creating a new project using one
- * of the templates to learn about Sprites, Tilemaps, Animations,
- * and more! Or check out the reference docs here:
- *
- * https://arcade.makecode.com/reference
- */
+let rotatableImage = new RotatableImage(assets.image`testImg`);
+let rotatableSprite = sprites.create(rotatableImage.currentImg);
+
+let angle = 0;
+let pFrameTime = game.runtime();
 
 game.onUpdate(() => {
-    // Code in this function will run once per frame. MakeCode
-    // Arcade games run at 30 FPS
-});
+    rotatableImage.rotate(angle);
+    rotatableSprite.setImage(rotatableImage.currentImg);
+    angle += 3;
+    info.setScore(1000 / (game.runtime() - pFrameTime));
+    pFrameTime = game.runtime();
+})
