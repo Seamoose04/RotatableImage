70f56f13e9888e2b0704f5a526c7278e406f25ca
Seamus Johnson <SMJohnson6@student.fullsail.edu>
2024-10-01 12:54:59 -0400

    fixed block fields

--- block-definitions.ts
+++ block-definitions.ts
@@ -4,13 +4,13 @@
 
 //% color=#0080ff icon="↻"
 namespace Rotatable {
-    //% block="$img"
+    //% block
     //% img.defl="screen_image_picker"
     export function createRotatableImage(img: Image): RotatableImage {
         return new RotatableImage(img);
     }
 
-    //% block="$rot_img, degrees"
+    //% block
     export function rotateImage(rot_img: RotatableImage, degrees: number): void {
         rot_img.rotate(degrees);
     }
